fe0b5c4374933b02e687972b1134df01b26c42dc
emileten <e.tenezakis@gmail.com>
2023-06-22 18:20:30 +0900

    preserve the ingest param so that the cogify workflow has access to it

--- lambdas/inventory/handler.py
+++ lambdas/inventory/handler.py
@@ -70,6 +70,7 @@ def handler(event, context):
                 "user_shared": event.get("user_shared", False),
                 "properties": event.get("properties", None),
                 "product_id": os.path.splitext(filename)[0].split("/")[-1],
+                "ingest": event.get("ingest", True)
             }
             if metadata_filename and metadata_type:
                 file_obj["assets"] = {metadata_type: metadata_filename}
